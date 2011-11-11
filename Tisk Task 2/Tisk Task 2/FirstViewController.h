cee1e70c6f118bc7b3979255382ade72e5119dff
Jordan Zucker <jordan.zucker@gmail.com>
2011-11-11 16:33:09 -0600

    Created new risk task project to try better on the second time

---
+++ Tisk Task 2/Tisk Task 2/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Tisk Task 2
+//
+//  Created by Jordan Zucker on 11/11/11.
+//  Copyright (c) 2011 University of Illinois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
