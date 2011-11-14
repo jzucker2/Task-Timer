8b0fd85ad27071c36cfa65e5acacbce8c5fcb00a
Jordan Zucker <jordan.zucker@gmail.com>
2011-11-13 20:56:39 -0600

    New object model

--- Tisk Task/Tisk Task/Task.h
+++ Tisk Task/Tisk Task/Task.h
@@ -2,7 +2,7 @@
 //  Task.h
 //  Tisk Task
 //
-//  Created by Jordan Zucker on 11/11/11.
+//  Created by Jordan Zucker on 11/13/11.
 //  Copyright (c) 2011 University of Illinois. All rights reserved.
 //
 
@@ -12,11 +12,13 @@
 
 @interface Task : NSManagedObject
 
+@property (nonatomic, retain) NSNumber * complete;
 @property (nonatomic, retain) NSNumber * current;
 @property (nonatomic, retain) NSNumber * duration;
 @property (nonatomic, retain) NSNumber * elapsed;
 @property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSNumber * running;
-@property (nonatomic, retain) NSNumber * complete;
+@property (nonatomic, retain) NSDate * startDate;
+@property (nonatomic, retain) NSDate * endDate;
 
 @end
--- Tisk Task/Tisk Task/Task.m
+++ Tisk Task/Tisk Task/Task.m
@@ -2,7 +2,7 @@
 //  Task.m
 //  Tisk Task
 //
-//  Created by Jordan Zucker on 11/11/11.
+//  Created by Jordan Zucker on 11/13/11.
 //  Copyright (c) 2011 University of Illinois. All rights reserved.
 //
 
@@ -11,11 +11,13 @@
 
 @implementation Task
 
+@dynamic complete;
 @dynamic current;
 @dynamic duration;
 @dynamic elapsed;
 @dynamic name;
 @dynamic running;
-@dynamic complete;
+@dynamic startDate;
+@dynamic endDate;
 
 @end
